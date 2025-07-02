ef266871e6fc9b119d875f162217c4ff379c3fa7
Minhaza <minhazaaa1006@gmail.com>
2025-07-02 23:36:49 +0700

    feat: Redesign login screen UI
    Updated activity_login.xml with a modern layout, new email icon assets, and a secondary button background. LoginActivity now displays a dynamic greeting based on user type passed via intent.

--- app/src/main/java/com/example/homehelperfinder/activities/LoginActivity.java
+++ app/src/main/java/com/example/homehelperfinder/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.homehelperfinder.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,9 @@ import com.example.homehelperfinder.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView tvGreeting;
+    private String userType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +26,35 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        userType = getIntent().getStringExtra("user_type");
+        initViews();
+    }
+
+    private void initViews() {
+        tvGreeting = findViewById(R.id.tvGreeting);
+        if (userType != null) {
+            switch (userType) {
+                case "customer":
+                    tvGreeting.setText("Hello Customer!");
+                    break;
+                case "helper":
+                    tvGreeting.setText("Hello Helper!");
+                    break;
+                case "admin":
+                    tvGreeting.setText("Hello Admin!");
+                    break;
+                default:
+                    tvGreeting.setText("Hello User!");
+                    break;
+            }
+        } else {
+            tvGreeting.setText("Hello User!");
+        }
+
     }
 }
