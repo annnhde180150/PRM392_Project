424977a249d5acda0adaf479c3f0d8368e16a0f3
hoangphuongphan <166013675+hoangphuongphan@users.noreply.github.com>
2025-07-10 21:59:25 +0700

    add front end for edit and delete request

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,5 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:3.4.1")
-
+    implementation ("com.google.android.material:material:1.11.0") // or latest
 }
